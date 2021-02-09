0d7697e611a8df2120e06127878c340bd810a795
Isak <isak_borgemo@hotmail.com>
2021-02-09 16:31:45 +0100

    Matematiska funktion funkar, men blir fel paket storlek när jag skickar

--- clientmain.cpp
+++ clientmain.cpp
@@ -7,7 +7,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 /* You will to add includes here */
-
+#include <calcLib.h>
 // Enable if you want debugging to be printed, see examble below.
 // Alternative, pass 
 #define DEBUG
@@ -37,17 +37,15 @@ int main(int argc, char *argv[]){
 	int rv;
 	int numbytes;
 	char buf[MAXDATASIZE];
-	char s[INET6_ADDRSTRLEN];
 
 	if (argc != 2) {
 	  fprintf(stderr,"usage: %s hostname (%d)\n",argv[0],argc);
 	  exit(1);
 	}
 
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE;
 
 	if ((rv = getaddrinfo(Desthost, Destport, &hints, &servinfo)) != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
@@ -84,31 +82,27 @@ int main(int argc, char *argv[]){
 	  exit(1);
 	}
 	
-	buf[numbytes] = '\0';
-	
 	printf("client: received '%s'\n",buf);
-	char lineBuffer[MAXDATASIZE];
-	char *lBptr=(char*)&lineBuffer;
-	size_t buffSize = MAXDATASIZE;
-	int lread;
+	char *lineBuffer = NULL;
+	size_t buffSize = 0;
+	ssize_t nread = 0;
+	char command[10];
+	double f1,f2,fresult;
+    int i1,i2,iresult;
+	
 	while(1){
 	  printf(":>");
-	  lread=getline(&lBptr, &buffSize,stdin);
-	  // Trunkate newline
-	  lineBuffer[lread-1]='\0';
+	  nread=getline(&lineBuffer, &buffSize,stdin);
 	  if(strcmp(lineBuffer,"exit")==0){
 	    printf("You'r leaving.\n");
 	    break;
 	  }
-	    
-	  printf("stdin read %d bytes, buffer = '%s' \n",lread, lineBuffer);
-	  if ((numbytes = send(sockfd, lineBuffer, lread, 0)) == -1) {
+	  if ((numbytes = send(sockfd, lineBuffer, nread, 0)) == -1) {
 	    perror("sendto:");
 	    exit(1);
 	  }
-	  printf("sent %d bytes\n", numbytes);
-
-	  if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+	
+	  if ((numbytes = recv(sockfd, lineBuffer, buffSize, 0)) == -1) {
 	    perror("recv");
 	    exit(1);
 	  }
@@ -116,18 +110,79 @@ int main(int argc, char *argv[]){
 	    printf("got zero.\n");
 	    break;
 	  }
-	  //	  buf[numbytes] = '\0';
-	  printf("client: received '%s/%d'\n",buf,numbytes);
-	}
-	close(sockfd);
+	  lineBuffer[numbytes] = '\0';
 
-	return 0;
+		printf("client: received '%s/%d'\n",lineBuffer,numbytes);
 
+	    rv=sscanf(lineBuffer,"%s",command);
+		#ifdef DEBUG 
+  		printf("Command: |%s|\n",command);
+		#endif
   
+  		if(command[0]=='f'){
+    		printf("Float\t");
+    		rv=sscanf(lineBuffer,"%s %lg %lg",command,&f1,&f2);
+    		if(strcmp(command,"fadd")==0){
+      		fresult=f1+f2;
+    		} else if (strcmp(command, "fsub")==0){
+      		fresult=f1-f2;
+    		} else if (strcmp(command, "fmul")==0){
+       		fresult=f1*f2;
+    		} else if (strcmp(command, "fdiv")==0){
+      		fresult=f1/f2;
+			printf("Result is %8.8g", fresult);
+			sprintf(lineBuffer, "%8.8g", fresult);
+    	}
+			#ifdef DEBUG
+			printf("Sent %8.8g \n", fresult);
+			#endif
+    		printf("%s %8.8g %8.8g = %8.8g\n",command,f1,f2,fresult);
+  		} else {
+    	printf("Int\t");
+    	rv=sscanf(lineBuffer,"%s %d %d",command,&i1,&i2);
+    	if(strcmp(command,"add")==0){
+      	iresult=i1+i2;
+    	} else if (strcmp(command, "sub")==0){
+     	 iresult=i1-i2;
+     	 printf("[%s %d %d = %d ]\n",command,i1,i2,iresult);
+    	} else if (strcmp(command, "mul")==0){
+      	iresult=i1*i2;
+    	} else if (strcmp(command, "div")==0){
+      
+      	iresult=i1/i2;
+		printf("Result is %d", iresult);
+		sprintf(lineBuffer, "%d", iresult);
+    } else {
+      printf("No match\n");
+    }
+	#ifdef DEBUG
+	printf("Sent %d \n", iresult);
+	#endif
+	//send(sockfd, &lineBuffer, strlen(lineBuffer), 0);
+    printf("%s %d %d = %d \n",command,i1,i2,iresult);
+  }
+  
+	send(sockfd, lineBuffer, strlen(lineBuffer), 0);
+	#ifdef DEBUG
+	printf("Sent %ld bytes\n", strlen(lineBuffer));
+	#endif
+	lineBuffer[numbytes] = '\0';
 
-/*#ifdef DEBUG 
-  printf("Host %s, and port %d.\n",Desthost,port);
-#endif*/
+	if ((numbytes = recv(sockfd, lineBuffer, buffSize, 0)) == -1) {
+	    perror("recv");
+	    exit(1);
+	  }
+	  if(numbytes==0){
+	    printf("got zero.\n");
+	    break;
+	  }
+	  lineBuffer[numbytes] = '\0';
 
+		printf("client: received '%s/%d'\n",lineBuffer,numbytes);
+	}
+	free(lineBuffer);
+	close(sockfd);
+	 
+	return 0;
   
 }
